76ce835b76335146ce82c5e0f260659c079e98b0
Angel Senra <yadkee@gmail.com>
2020-06-07 00:00:50 +0200

    Count every route change

--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,38 @@
-import React from 'react';
+import React from 'react'
 import ReactDOM from 'react-dom';
 import './css/index.css';
 import App from './app.jsx';
 import Blog from './blog.jsx';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter, Route, withRouter } from "react-router-dom";
+
+
+class Container extends React.Component {
+  componentDidMount() {
+    // https://github.com/ReactTraining/react-router/issues/3554
+    this.unlisten = this.props.history.listen((location, action) => {
+      window.goatcounter.count()
+    })
+  }
+
+  componentWillUnmount() {
+    this.unlisten()
+  }
+
+  render() {
+    return this.props.children
+  }
+}
+const Decorated = withRouter(Container)
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <div>
+    <BrowserRouter>
+      <Decorated>
         <Route path="/" exact component={App} />
         <Route path="/blog/" component={Blog} />
-      </div>
-    </Router>
+      </Decorated>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
