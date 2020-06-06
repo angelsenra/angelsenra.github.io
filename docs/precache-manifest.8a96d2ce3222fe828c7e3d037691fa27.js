908f3a7572aec4648627c6dbf8744bf277a0a979
Angel Senra <yadkee@gmail.com>
2020-06-06 20:19:34 +0200

    Publish artifact in /docs

---
+++ docs/precache-manifest.8a96d2ce3222fe828c7e3d037691fa27.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f326d660bc15ee1ae47e10754ac10859",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1eb1982f5f6a8df4a15c",
+    "url": "/static/css/main.f7b76ced.chunk.css"
+  },
+  {
+    "revision": "7a36b48f3a8baa9e67bf",
+    "url": "/static/js/2.be4fa119.chunk.js"
+  },
+  {
+    "revision": "b013b7cd7a2da92c16a2d9932cfeb76c",
+    "url": "/static/js/2.be4fa119.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1eb1982f5f6a8df4a15c",
+    "url": "/static/js/main.6880fe89.chunk.js"
+  },
+  {
+    "revision": "d054257743b49392b64b",
+    "url": "/static/js/runtime-main.7ac1886d.js"
+  },
+  {
+    "revision": "39d4a3ffa80f964f118cdbc9c8753670",
+    "url": "/static/media/me.39d4a3ff.png"
+  }
+]);
