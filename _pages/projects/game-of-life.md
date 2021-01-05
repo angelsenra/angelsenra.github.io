5ace8c48b6ed190f1f7d63308224dec062da3cb0
Angel Senra <git@angelsenra.com>
2021-01-05 02:28:03 +0100

    Add buttons to game of life

--- _pages/projects/game-of-life.md
+++ _pages/projects/game-of-life.md
@@ -6,25 +6,40 @@ title: "Conway's Game of Life: the most famous cellular automaton"
 description: >-
   Building a Game of Life using a canvas and javascript code. It's my first project using canvas and it's mostly an excuse to learn it
 last_modified_at: 2020-12-29
+uses_bootstrap: true
 ---
 
-# [Home]({% link _pages/index.md %}) > [Projects]({% link _pages/projects.md %}) > Game of Life
+# [Home]({% link _pages/index.md %}) > [Projects]({% link _pages/projects.md %}) > Game&nbsp;of&nbsp;Life
 
 <div class="h-centered-text">
   <canvas id="game-canvas" width="600" height="400"></canvas>
 </div>
 
-<h1 id="paused" class="h-centered-text hidden">-- PAUSED --</h1>
+<div class="h-centered-text">
+  <p>
+    Updating <span id="game-speed">??</span> times per second -
+    <span id="step-count">??</span> steps since last reset
+  </p>
 
-Updating <span id="game-speed">??</span> times per second -
-<span id="step-count">??</span> steps since last reset
+  <div class="btn-group">
+    <button class="btn btn-primary" id="btn-pause">Pause</button>
+    <button class="btn btn-primary" id="btn-next">Next Step</button>
+    <button class="btn btn-primary" id="btn-speed-up">Speed Up</button>
+    <button class="btn btn-primary" id="btn-slow-down">Slow Down</button>
+    <button class="btn btn-primary" id="btn-reset">Reset</button>
+    <button class="btn btn-primary" id="btn-clear">Clear</button>
+  </div>
+</div>
+
+<br>
 
 ```
 Pause       - space / p
-Next step   - right arrow / d / n
+Next Step   - right arrow / d / n
 Speed Up    - up arrow / w
-Slow Down  - down arrow / s
+Slow Down   - down arrow / s
 Reset       - r
+Clear       - c
 ```
 
 <script src="{% link assets/game-of-life.js %}"></script>
--- assets/game-of-life.js
+++ assets/game-of-life.js
@@ -1,7 +1,12 @@
 const canvas = document.getElementById("game-canvas");
+const buttonPause = document.getElementById("btn-pause");
+const buttonNext = document.getElementById("btn-next");
+const buttonSpeedUp = document.getElementById("btn-speed-up");
+const buttonSlowDown = document.getElementById("btn-slow-down");
+const buttonReset = document.getElementById("btn-reset");
+const buttonClear = document.getElementById("btn-clear");
 const gameSpeedSpan = document.getElementById("game-speed");
 const stepCountSpan = document.getElementById("step-count");
-const pausedTitle = document.getElementById("paused");
 const ctx = canvas.getContext("2d");
 const BLOCK_SIZE = 10;
 canvas.width -= canvas.width % BLOCK_SIZE;
@@ -108,21 +113,18 @@ const keyDownHandler = (e) => {
     lifeStep();
   }
   if (e.key == "ArrowUp" || e.key == "w") {
-    gameSpeed = Math.min(gameSpeed + 1, 60);
-    gameSpeedSpan.innerHTML = gameSpeed;
+    speedUp();
     e.preventDefault(); // Don't scroll up on arrow press
   }
   if (e.key == "ArrowDown" || e.key == "s") {
-    gameSpeed = Math.max(gameSpeed - 1, 1);
-    gameSpeedSpan.innerHTML = gameSpeed;
-    e.preventDefault();
+    slowDown();
+    e.preventDefault(); // Don't scroll dowb on arrow press
   }
 };
 
 const keyUpHandler = (e) => {
   if (e.key == " " || e.key == "p") {
-    isPaused ^= 1;
-    pausedTitle.style.display = isPaused ? "block" : "none";
+    togglePause();
   }
   if (e.key == "r") {
     randomGrid();
@@ -141,14 +143,37 @@ const mouseDownHandler = (e) => {
   drawCell(x, y);
 };
 
+const togglePause = () => {
+  isPaused ^= 1;
+  buttonPause.innerHTML = isPaused ? "Continue" : "Pause";
+};
+
+const speedUp = () => {
+  gameSpeed = Math.min(gameSpeed + 1, 60);
+  gameSpeedSpan.innerHTML = gameSpeed;
+};
+
+const slowDown = () => {
+  gameSpeed = Math.max(gameSpeed - 1, 1);
+  gameSpeedSpan.innerHTML = gameSpeed;
+};
+
 // Main
 
-randomGrid();
-gameLoop();
-drawLoop();
-gameSpeedSpan.innerHTML = gameSpeed;
-stepCountSpan.innerHTML = stepCount;
+window.onload = () => {
+  randomGrid();
+  gameLoop();
+  drawLoop();
+  gameSpeedSpan.innerHTML = gameSpeed;
+  stepCountSpan.innerHTML = stepCount;
 
-document.addEventListener("keydown", keyDownHandler, false);
-document.addEventListener("keyup", keyUpHandler, false);
-canvas.addEventListener("mousedown", mouseDownHandler, false);
+  document.addEventListener("keydown", keyDownHandler, false);
+  document.addEventListener("keyup", keyUpHandler, false);
+  canvas.addEventListener("mousedown", mouseDownHandler, false);
+  buttonPause.addEventListener("click", togglePause, false);
+  buttonNext.addEventListener("click", lifeStep, false);
+  buttonSpeedUp.addEventListener("click", speedUp, false);
+  buttonSlowDown.addEventListener("click", slowDown, false);
+  buttonReset.addEventListener("click", randomGrid, false);
+  buttonClear.addEventListener("click", clearGrid, false);
+};
